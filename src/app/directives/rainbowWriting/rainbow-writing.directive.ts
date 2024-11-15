a259a9b96115845c307a82c08790f5fadcce1b37
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2024-11-15 12:38:35 +0100

    add rainbow writing directive + test component

---
+++ src/app/directives/rainbowWriting/rainbow-writing.directive.ts
@@ -0,0 +1,53 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appRainbowWriting]',
+  standalone: true
+})
+export class RainbowWritingDirective {
+  private colors: string[] = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet',
+  ];
+
+  @HostBinding('style.color') textColor!: string;
+  @HostBinding('style.borderColor') borderColor!: string;
+
+  constructor(private el: ElementRef) {
+    this.initRainbowEffect();
+  }
+
+  private initRainbowEffect(): void {
+    if (!(this.el.nativeElement instanceof HTMLInputElement)) {
+      console.error(`cette directive ne peut etre utilisé pour l element de type ${this.el.nativeElement}.`);
+      return;
+    }
+    const keyup$ = fromEvent(this.el.nativeElement, 'keyup');
+
+    keyup$
+      .pipe(
+        map(() => this.getRandomColor())
+      )
+      .subscribe((color) => {
+        this.textColor = color;
+        this.borderColor = color;
+      });
+  }
+
+  private getRandomColor(): string {
+    const index = Math.floor(Math.random() * this.colors.length);
+    return this.colors[index];
+  }
+}
