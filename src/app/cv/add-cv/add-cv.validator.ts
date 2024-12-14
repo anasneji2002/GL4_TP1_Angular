00a68ce4bdb11ddf30dcd873dc0c615e85372c32
AhmedCh24 <ahmedcherif2024@gmail.com>
2024-12-14 08:11:54 +0100

    minor changes

--- src/app/cv/add-cv/add-cv.validator.ts
+++ src/app/cv/add-cv/add-cv.validator.ts
@@ -1,24 +1,25 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+//import { AddCvComponent } from "./add-cv.component";
 
 export function cinAgeValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const age = control.get('age')?.value;
-    const cin = control.get('cin')?.value;
+  return (form: AbstractControl): ValidationErrors | null => {
+    const age = form.get('age')?.value;
+    const cin = form.get('cin')?.value;
 
-    if (age == null || cin == null || cin.length < 2) {
-      return null; // Pas de validation si les champs ne sont pas complets
+    if (age == null || cin == null || cin.length < 8) {
+      return null; 
     }
 
     const firstTwoDigits = parseInt(cin.substring(0, 2), 10);
 
-    if (age >= 60 && (firstTwoDigits < 0 || firstTwoDigits > 19)) {
-      return { cinFirstCars: 'Les deux premiers chiffres du CIN doivent être entre 00 et 19 pour les personnes de 60 ans ou plus.' };
+    if (age >= 60 && firstTwoDigits > 19) {
+      return { cinFirstCars: 'l zouz lawlenin mtaa l cin lezem byyn  00 et 19 la3mor akther mel 60.' };
     }
 
     if (age < 60 && firstTwoDigits <= 19) {
-      return { cinFirstCars: 'Les deux premiers chiffres du CIN doivent être supérieurs à 19 pour les personnes de moins de 60 ans.' };
+      return { cinFirstCars: 'l zouz lawlenin mtaa l cin lezem akther men 19 la3mor a9al mel 60.' };
     }
 
-    return null; // Pas d'erreur
+    return null; 
   };
 }
